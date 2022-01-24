5feb61cef2a5007f382e8f11a1c14e9c9de546a0
hd972392 <harshdumaniya972392@gmail.com>
2022-01-24 18:55:51 +0530

    core example

---
+++ Person.java
@@ -0,0 +1,29 @@
+package com.hd.core;
+
+/**Practical 11 : using this variable
+ * @author Harsh
+ * @version 1.0
+ * @since 24-01-2022
+ */
+class Person {
+	int age;
+	String name;
+	Person(int age,String name) {
+		this.age=age;
+		this.name=name;
+	}
+	void show() {
+		System.out.println(age+" "+name);
+	}
+}
+
+class TestThis{
+
+public static void main(String args[]){
+	Person test1=new Person(20,"Suresh");
+	Person test2=new Person(21,"Mahesh");
+	test1.show();
+	test2.show();
+	}
+
+}
