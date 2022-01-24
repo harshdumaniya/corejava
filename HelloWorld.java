21aa8963bc95fe35e7a7b8e1d48666d3b53a3117
hd972392 <harshdumaniya972392@gmail.com>
2022-01-24 12:45:43 +0530

    Core example

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package com.hd.core;
+/**this is a program to print hello world in the output using standard java class */
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		
+		/*write a java standard class for print the output*/
+			System.out.println("hello World");//print helloworld
+		
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.hd.core;
+public class Main {
+
+	public static void main(String args[]) {
+		int age = 19;
+		if (age > 18) {
+			System.out.println("You are eligibe for voting");
+		} else {
+			System.out.println("You are not eligibe for voting");
+		}
+	}
+
+}
