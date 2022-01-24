83d32d0739b05faf26e303d9d4c316fa4dc44e59
hd972392 <harshdumaniya972392@gmail.com>
2022-01-24 20:10:53 +0530

    core example

--- Person.java
+++ Person.java
@@ -5,19 +5,19 @@ class Person {
 	int age;
 	String name;
 	Person(int age, String name) {
-	this.age = age;
-	this.name = name;
+		this.age = age;
+		this.name = name;
 	}
 	void show() {
-		System.out.println(age+" "+name);
+		System.out.println(age +" "+ name);
 	}
 
 }
 class PersonDetails {
 
 	public static void main(String args[]) {
-		Person person1 = new Person(20 , "Suresh");
-		Person person2 = new Person(21 , "Mahesh");
+		Person person1 = new Person(20, "Suresh");
+		Person person2 = new Person(21, "Mahesh");
 		person1.show();
 		person2.show();
 	}
