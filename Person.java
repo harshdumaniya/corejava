22fb95a8d7ed9555af0a03dc29dad3f1a87beee9
hd972392 <harshdumaniya972392@gmail.com>
2022-01-24 18:58:55 +0530

    core example

--- Person.java
+++ Person.java
@@ -16,10 +16,11 @@ class Person {
 		System.out.println(age+" "+name);
 	}
 }
+package com.hd.core;
 
 class TestThis{
 
-public static void main(String args[]){
+public static void main(String args[]) {
 	Person test1=new Person(20,"Suresh");
 	Person test2=new Person(21,"Mahesh");
 	test1.show();
