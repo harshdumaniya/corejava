db773148e56702922a859b93bd6ae027a1b6dd56
hd972392 <harshdumaniya972392@gmail.com>
2022-01-24 17:12:35 +0530

    core example

--- EmiCalc.java
+++ EmiCalc.java
@@ -1,11 +1,34 @@
 package com.hd.core;
 
+import java.util.Scanner;
+
 public class EmiCalc {
 
+	public void calc(float p,float r,float t) {
+		float principle = p;
+		float rate = r;
+		float time = t;
+		
+		rate = rate / (12 * 100);
+		time = time * 12;
+		float emi = (( p * r * (float) Math.pow (1 + r, t )) / ((float) Math.pow ( 1 + r, t ) -1 )); 
+		System.out.print("EMI is: "+emi+"\n");
+		}
+
 	public static void main(String[] args) {
-		double principal = 10000, rate = 2 , time = 1, emi;
-		emi= (principal * rate * Math.pow(1 + rate , time))/(Math.pow(1 + rate, time) - 1);
-		System.out.println("Monthly EMI is :" +emi);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the principle amount: ");
+		float principle = sc.nextFloat();
+
+		System.out.print("Enter the rate of intrest:");
+		float rate = sc.nextFloat();
+
+		System.out.print("Enter the time period:");
+		float time = sc.nextFloat();
+
+		EmiCalc emi = new EmiCalc();
+		emi.calc(principle,rate,time);
 	}
 
 }
